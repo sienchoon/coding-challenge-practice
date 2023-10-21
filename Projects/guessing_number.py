3f73aafc2340cf825c4a8425ec566a286ee222d8
sienchoon <wongchoonsien@gmail.com>
2023-10-21 21:56:08 +1100

    21-10

---
+++ Projects/guessing_number.py
@@ -0,0 +1,24 @@
+import random
+
+#guessing number game
+
+print("I am thinking of a number between 1 and 20")
+print("Can you guess the number?")
+n = random.randrange(1, 20)
+guess = int(input("Enter a number: "))
+attempt = 1
+
+while n!= guess:
+    if guess < n:
+        print("Too low")
+        guess = int(input("Enter a number: "))
+        attempt += 1
+    elif guess > n:
+        print("Too high")
+        guess = int(input("Enter a number: "))
+        attempt += 1
+    else:
+        break
+
+print("You got it!")
+print(f"The number was {n} and it took you {str(attempt)} attempts")
---
+++ Projects/monthly_income.py
@@ -0,0 +1,6 @@
+#Project Title:  Monthly income calculator
+
+#how many income from jobs, frequency (weekly/fortnightly/monthly)
+#gross income vs income after tax
+#tax rate
+#date of payment
