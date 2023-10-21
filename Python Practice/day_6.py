0099344435c9b87bbe5d783007dfdb9dddfa70e8
sienchoon <wongchoonsien@gmail.com>
2023-10-21 21:15:43 +1100

    start

--- Python Practice/day_6.py
+++ Python Practice/day_6.py
@@ -1,7 +1,27 @@
-string = input("Enter a word of choice: ")
-even_strings = []
-def get_even_strings(string):
-    for even_index in string:
-        if even_index % 2 == 0:
-            even_strings.append(even_index)
-    return even_strings
+# user_input = input("Enter a word of choice: ")
+# even_characters = []
+# def get_even_characters(input_string):
+#     for index in range(len(input_string)):
+#         if index % 2 == 0:
+#             even_characters.append(input_string[index])
+#     return even_characters
+
+# if len(user_input) == 0:   
+#     print("Please enter a valid word...")
+# else:
+#     print("The even characters in the words:", ''.join(user_input))
+    
+user_input = input("Enter a word of choice: ")
+even_characters = []
+
+def get_even_characters(input_string):
+    for index, char in enumerate(input_string):
+        if index % 2 == 0:
+            even_characters.append(char)
+    return even_characters
+
+if len(user_input) == 0:
+    print("Please enter a valid word.")
+else:
+    even_characters = get_even_characters(user_input)
+    print("Even characters in the word:", ''.join(even_characters))
---
+++ Python Practice/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+user_input = input("Enter a word of choice: ")
+# even_characters = []
+# def get_even_characters(input_string):
+#     for index in range(len(input_string)):
+#         if index % 2 == 0:
+#             even_characters.append(input_string[index])
+#     return even_characters
+
+# if len(user_input) == 0:   
+#     print("Please enter a valid word...")
+# else:
+#     print("The even characters in the words:", ''.join(user_input))
