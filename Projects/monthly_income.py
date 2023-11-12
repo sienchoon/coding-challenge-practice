86e281629473c053fe3f6f69c939d9e62ee7ee25
sienchoon <wongchoonsien@gmail.com>
2023-11-13 00:31:13 +1100

    income

--- Projects/monthly_income.py
+++ Projects/monthly_income.py
@@ -8,13 +8,39 @@
 
 
 
-def income():
-    income = input("Enter the amount of income: $")
-    job = input("Which job did you get income from: ").title()
-    paydate = input("Enter the date of payment(DD/MM/YYYY): ")
-    print(f'Your income from {job} is ${income} and was paid on {paydate}')
+# def income():
+#     income = input("Enter the amount of income: $")
+#     job = input("Which job did you get income from: ").title()
+#     paydate = input("Enter the date of payment(DD/MM/YYYY): ")
+#     print(f'Your income from {job} is ${income} and was paid on {paydate}')
+def month():
+    month = input("Enter the month: ")
+    month = month.title()
+    return f"The current month is: {month}"
 
+month() 
 
+def job_income():
+    jobs = []
+    income = []
+    counter = 0
+    
+    while True:
+        job_input = input("Enter the job name (or enter 'q' to finish): ")
+        job_input = job_input.title()
+        if job_input.lower() == 'q':
+            break
+        else:
+            jobs.append(job_input)
+            counter +=1 
+            
+    print("Here are list of jobs you have entered: \n")
+    counter = 1
+    for job in jobs:
+        print(f"{counter}. {job}")
+        counter += 1
 
+job_income()
 
+    
 
--- Python Practice/day_8_classes.py
+++ Python Practice/day_8_classes.py
@@ -1,6 +1,5 @@
 #classes practice
-
-class Car():
+class Car(): 
     def __init__(self, make, model, year):
         self.make = make
         self.model = model
@@ -8,7 +7,7 @@ class Car():
         self.odometer_reading = 0 #default value 
     def get_descriptive_name(self):
         long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
+        print(long_name.title())
     def read_odometer(self):
         print(f"This car has {self.odometer_reading} miles on it.")
     def update_odometer(self, mileage):
@@ -20,26 +19,38 @@ class Car():
         self.odometer_reading += miles
     def fill_gas_tank(self):
         print("This car doesn't need a gas tank!")
-
+        
 #create a variable attached to an object eg, car1, car2 etc        
 car1 = Car('audi', 'a4', 2016)
-
 #attach method to variable
-print(car1.get_descriptive_name())
-print(car1.read_odometer())
-print(car1.fill_gas_tank())
-print(car1.increment_odometer(100))
+car1.get_descriptive_name()
 
-print("-----------------------------------------------------------------------\n")
+print("\n-----------------------------------------------------------------------")
 
 class Hero():
     def __init__(self, name, power):
         self.name = name
         self.power = power
-    def attack(self):
-        print(f"{self.name} is attacking with {self.power} power")
-    def defend(self):
-        print(f"{self.name} is defending with {self.power} power")
+    def attack(self,attack):
+        print(f"{self.name} is attacking with {attack} power points.")
+        print(f"{self.name} has {self.power - attack} power points left.")
+    def defend(self, defend):
+        print(f"{self.name} is defending with {defend} power points.")
+        
+    def move_right(self, steps):
+        print(f"{self.name} is moving right, taking {steps} steps.") 
+    
+    def move_left(self, steps):
+        print(f"{self.name} is moving left, taking {steps} steps.")
+        
+    def move_up(self, steps):
+        print(f"{self.name} is moving up, taking {steps} steps.")
+        
+    def move_down(self, steps):
+        print(f"{self.name} is moving down, taking {steps} steps.")
+        
         
 hero1 = Hero("Spiderman", 100)
-hero1.attack()
+hero1.attack(20)
+hero1.move_down(5)
+
