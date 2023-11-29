6ae3ab8f3294f7c194404e2271c1c3b0adc94533
sienchoon <wongchoonsien@gmail.com>
2023-11-29 22:25:28 +1100

    practice

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -4,3 +4,16 @@ console.log(23+97)
 
 let addition = (a , b) => a + b
 console.log(addition(1,2))
+
+let trace1 = {
+    x: xData,
+    y: yData
+};
+  
+let data = [trace1];
+  
+let layout = {
+    title: "A Plotly plot"
+};
+  
+Plotly.newPlot("plot", data, layout);
