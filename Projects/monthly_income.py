ececbf849315129db71fc9010f884725b57fa979
sienchoon <wongchoonsien@gmail.com>
2023-12-09 18:09:05 +1100

    progress on python coding

--- Projects/monthly_income.py
+++ Projects/monthly_income.py
@@ -11,11 +11,17 @@
 #     job = input("Which job did you get income from: ").title()
 #     paydate = input("Enter the date of payment(DD/MM/YYYY): ")
 #     print(f'Your income from {job} is ${income} and was paid on {paydate}')
-def month(month):
-    month = input("Enter the month: ")
-    month = month.title()
-    print(f"The current month is: {month}")
-month(month) 
+def get_month():
+    while True:
+        months = 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+        month = input("Enter the month: ")
+        month = month.title()
+        if month in months:
+            return month
+        else:
+            print("Invalid month. Please enter a valid month.")
+        print(f"The current month is: {month}")        
+
 def job_income():
     jobs = []
     income = []   
@@ -25,26 +31,37 @@ def job_income():
         if job_input.lower() == 'q':
             break
         else:
-            try:
-                income_input = float(input(f"Enter the income for {job_input}: $"))
-            except ValueError:
-                print("Invalid input. Please enter a valid numeric income.")
-            
+            while True:
+                try:
+                    income_input = float(input(f"Enter the income for {job_input}: $"))
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter a valid numeric income.")
+           
             jobs.append(job_input)
             income.append(income_input)
-            # counter +=1 
-     
+            # counter +=1     
     return jobs, income
 
-def display_jobs(jobs, incomes):
-    print("Here are the income for the jobs you have entered: \n")
+def display_jobs(jobs, incomes, month):
+    print(f"Here are the income for the month of {month} you have entered: \n")
     for i in range(len(jobs)):
         print(f"{i + 1}. {jobs[i]} : ${incomes[i]:,.2f}\n")
+    print(f"The total income for the month is: ${sum(incomes):,.2f}\n")
+
+def write_output_file(selected_month, jobs, income, file_name='monthly income.txt'):
+    with open(file_name, 'a') as file:
+        file.write(f"Here are the income for the month of {selected_month} you have entered: \n")
+        for i in range(len(jobs)):
+            file.write(f"{i + 1}. {jobs[i]} : ${income[i]:,.2f}\n")
+        file.write(f"The total income for the month is: ${sum(income):,.2f}\n")
 
 
-job_list, income_list = job_income()
-display_jobs(job_list, income_list)
-
-
-    
-
+# execute the functions below (main execution)
+if __name__ == "__main__" :
+    selected_month = get_month()
+# job_list, income_list = job_income()
+    jobs, income = job_income()
+# display_jobs(job_list, income_list, month)
+    display_jobs(jobs, income ,selected_month)
+    write_output_file(selected_month, jobs, income)
