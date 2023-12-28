393cf4ba54cd68529c861f46131f5b4043cdeff3
sienchoon <wongchoonsien@gmail.com>
2023-12-28 21:49:28 +1100

    income tracker project

--- Projects/Income_Project/template/app_display.js
+++ Projects/Income_Project/template/app_display.js
@@ -3,95 +3,154 @@
 document.addEventListener('DOMContentLoaded', function () {
     const incomeInput = document.getElementById('income');
     const dateInput = document.getElementById('date');
-    const jobSelect = document.getElementById('job');
-    const incomeList = document.getElementById('income-list');
+    const jobSelect = document.getElementById('jobSelect');
+    const otherJobInputBox = document.getElementById('otherJobInputBox');
+    const otherJobInput = document.getElementById('otherJob');
+    // const incomeList = document.getElementById('income-list');
+    const incomeTable = document.getElementById('income-table');
+    const incomeTableBody = document.getElementById('income-table-body'); 
     const addButton = document.getElementById('add-income');
     const exportButton = document.getElementById('export-data');
-    const pieChartCanvas = document.getElementById('pie-chart');
-    
-    addButton.addEventListener('click', function () {
+
+        // Function to toggle the visibility of the otherJobInputBox based on the selected value
+        function toggleOtherJobInput() {
+            const selectedJob = jobSelect.value;
+            if (selectedJob === 'Other') {
+                otherJobInputBox.style.display = 'block';
+            } else {
+                otherJobInputBox.style.display = 'none';
+            }
+        }
+
+        // Attach the toggleOtherJobInput function to the change event of the jobSelect element
+        jobSelect.addEventListener('change', toggleOtherJobInput);
+
+    // Initialize an empty object to store monthly incomes by job
+    const monthlyIncomesByJob = {};
+
+        addButton.addEventListener('click', function () {
         const incomeValue = parseFloat(incomeInput.value.trim());
         const dateValue = dateInput.value.trim();
-        const jobValue = jobSelect.value.trim();
+        let jobValue;
+
+        if (jobSelect.value === 'Other') {
+            // Use the value from the otherJobInput if 'Other' is selected
+            jobValue = otherJobInput.value.trim();
+        } else {
+            jobValue = jobSelect.value.trim();
+        }
 
         if (!incomeValue || isNaN(incomeValue) || !dateValue || !jobValue) {
             alert('Invalid value. Please enter a valid income and fill in all fields.');
             return;
         }
 
-        const listItem = document.createElement('li');
-        listItem.textContent = `Income: $${incomeValue.toFixed(2)}, Date: ${dateValue}, Job: ${jobValue}`;
-        incomeList.appendChild(listItem);
+        // Update the monthlyIncomesByJob object
+        const monthYear = new Date(dateValue).toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' });
+        const key = `${monthYear}-${jobValue}`;
+
+        if (monthlyIncomesByJob[key]) {
+            monthlyIncomesByJob[key] += incomeValue;
+        } else {
+            monthlyIncomesByJob[key] = incomeValue;
+        }
+
+
+        // THIS CODE FOR LISTING INCOMES-----------------------------------------------------------------------------------------------
+        // const listItem = document.createElement('li');
+        // listItem.textContent = `Income: $${incomeValue.toFixed(2)}, Date: ${dateValue}, Job: ${jobValue}`;
+        // incomeList.appendChild(listItem);
+
+        // incomeInput.value = '';
+        // dateInput.value = '';
+        // jobSelect.value = '';
+        //--------------------------------------------------------------------------------------------------------------------------------
+
+        // Create a new row in the table
+        const newRow = incomeTableBody.insertRow();
+        
+        // Add cells to the row
+        // const indexCell = newRow.insertCell(0);
+        const dateCell = newRow.insertCell(0);
+        const incomeCell = newRow.insertCell(1);
+        const jobCell = newRow.insertCell(2);
+
+        // Assign values to the cells
+        // indexCell.textContent = incomeTableBody.children.length;
+        incomeCell.textContent = `$${incomeValue.toFixed(2)}`;
+        dateCell.textContent = monthYear;
+        jobCell.textContent = jobValue;
 
         incomeInput.value = '';
         dateInput.value = '';
         jobSelect.value = '';
-        updatePieChart();
+        otherJobInput.value = ''; // Clear the otherJobInput value
     });
+
+ 
+// // export the data when it is in the list format <ol> tag
+// exportButton.addEventListener('click', function () {
+//     // Create a string with the content and indices
+//     let content = '';
+//     const items = incomeList.children;
+
+//     for (let i = 0; i < items.length; i++) {
+//         content += `${i + 1}. ${items[i].textContent}\n`;
+//     }
+//     // Create a Blob with the content
+//     const blob = new Blob([content], { type: 'text/plain' });
+
+//     // Create a link element
+//     const link = document.createElement('a');
+//     link.href = URL.createObjectURL(blob);
+
+//     // Set the filename for the download
+//     link.download = 'income_data.txt';
+
+//     // Append the link to the document
+//     document.body.appendChild(link);
+
+//     // Trigger a click on the link to start the download
+//     link.click();
+
+//     // Remove the link from the document
+//     document.body.removeChild(link);
+
+//     });
+// });
 
 exportButton.addEventListener('click', function () {
-    // Create a string with the content and indices
-    let content = '';
-    const items = incomeList.children;
+    console.log('Button clicked!'); 
+    // Create a string with the content and indices from the table
+    let content = 'Date\tIncome\tJob\n'; // Add column headers
 
-    for (let i = 0; i < items.length; i++) {
-        content += `${i + 1}. ${items[i].textContent}\n`;
+    const rows = incomeTableBody.children;
+    for (let i = 0; i < rows.length; i++) {
+        const cells = rows[i].cells;
+        const date = cells[0].textContent;
+        const income = cells[1].textContent.slice(1); // Remove the "$" sign
+        const job = cells[2].textContent;
+
+        content += `${date}\t${income}\t${job}\n`;
     }
-    // Create a Blob with the content
-    const blob = new Blob([content], { type: 'text/plain' });
 
-    // Create a link element
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
+        // Create a data URI for the text file
+        const dataUri = 'data:text/plain;charset=utf-8,' + encodeURIComponent(content);
 
-    // Set the filename for the download
-    link.download = 'income_data.txt';
+        // Create a link element
+        const link = document.createElement('a');
+        link.href = dataUri;
 
-    // Append the link to the document
-    document.body.appendChild(link);
+        // Set the filename for the download
+        link.download = 'income_data.txt';
 
-    // Trigger a click on the link to start the download
-    link.click();
+        // Append the link to the document
+        document.body.appendChild(link);
 
-    // Remove the link from the document
-    document.body.removeChild(link);
+        // Trigger a click on the link to start the download
+        link.click();
 
+        // Remove the link from the document
+        document.body.removeChild(link);
     });
 });
-
-
-function updatePieChart() {
-    const jobLabels = Object.keys(incomesByJob);
-    const incomeValues = Object.values(incomesByJob);
-
-    // Check if the pie chart has been initialized
-    if (!window.myPieChart) {
-        // Initialize the pie chart
-        window.myPieChart = new Chart(pieChartCanvas, {
-            type: 'pie',
-            data: {
-                labels: jobLabels,
-                datasets: [{
-                    data: incomeValues,
-                    backgroundColor: getRandomColors(jobLabels.length),
-                }],
-            },
-        });
-    } else {
-        // Update the data of the existing pie chart
-        window.myPieChart.data.labels = jobLabels;
-        window.myPieChart.data.datasets[0].data = incomeValues;
-        window.myPieChart.data.datasets[0].backgroundColor = getRandomColors(jobLabels.length);
-        window.myPieChart.update();
-    }
-}
-
-function getRandomColors(count) {
-    const colors = [];
-    for (let i = 0; i < count; i++) {
-        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        colors.push(randomColor);
-    }
-    return colors;
-};
-
